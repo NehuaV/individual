bd10b70bdf0b68e70f217ce07fefaa9bc3b54da5
NehuaV <79987000+NehuaV@users.noreply.github.com>
2021-09-21 14:18:07 +0200

    Add temporary UnitTest

---
+++ src/test/java/com/example/Player/UnitTest.java
@@ -0,0 +1,12 @@
+package com.example.Player;
+
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UnitTest {
+    @Test
+    public void Test1(){
+        Assertions.assertEquals(1,1);
+    }
+}
