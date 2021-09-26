05b47af536ff9d37ad2312403e75335ec5959bcc
NehuaV <79987000+NehuaV@users.noreply.github.com>
2021-09-27 00:04:47 +0200

    Basic React

---
+++ react/react-frontend/src/component/UserComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import UserService from '../services/UserService';
+
+class UserComponent extends React.Component {
+
+    constructor(){
+        this.state = {
+            users:[]
+        }
+    }
+    
+    render() {
+        return{
+            <div>
+            </div>
+        }
+    }
+}
+
+export default UserComponent
