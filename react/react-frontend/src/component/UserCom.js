1d49f6def016905ef55ebf751e9708c211a8a6e8
NehuaV <79987000+NehuaV@users.noreply.github.com>
2021-09-30 13:37:33 +0200

    React Basic

---
+++ react/react-frontend/src/component/UserCom.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class UserCom extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             posts : []
+        }
+    }
+    
+    
+    componentDidMount() {
+        axios.get('http://localhost:8080/users')
+        .then(response =>{
+            this.setState({
+                post: response.data
+            })
+            console.log(response.data)
+        })
+    }
+
+    render() {
+        const {posts} = this.state
+        return(
+            <div>
+                <h1>List Of Users</h1>
+                {
+                    posts.map(post => <div key={post.userID}>{post.username}</div>)
+                }
+            </div>
+        )
+    }
+}
+
+export default UserCom
