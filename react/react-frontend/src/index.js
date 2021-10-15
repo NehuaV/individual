ca81730573cfbff8f500fa5e6e81a4f4fac3df89
NehuaV <79987000+NehuaV@users.noreply.github.com>
2021-10-15 13:32:16 +0200

    Visual Upgrade 1
    Adding components and updating basic visual structure of the front end

--- react/react-frontend/src/index.js
+++ react/react-frontend/src/index.js
@@ -11,6 +11,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+document.body.style.overflow ="hidden";
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
