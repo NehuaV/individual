b6673fbfac78b193f099ebfa42dcb210d6cc5708
NehuaV <79987000+NehuaV@users.noreply.github.com>
2021-10-20 22:35:51 +0200

    Collection of data and style changes
    -The app now accepts a collection of HTTPS link as music source.
    -Small visual changes that will be implemented in the final look of the application

--- react/react-frontend/src/component/PlayerReact.js
+++ react/react-frontend/src/component/PlayerReact.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import styles from "../css/PlayerReact.css";
 
 export default class PlayerReact extends React.Component {
-  constructor() { 
+  constructor() {
     super();
-    this.state = { // State Reloads Component every time the state has been changed
-      url: "https://www.youtube.com/watch?v=Sv8LHpezbLw&t=15s",
+    this.state = {
+      // State Reloads Component every time the state has been changed
+
+      // Player functionality
+      url: null,
       pip: false,
       playing: false,
       controls: false,
@@ -17,48 +21,81 @@ export default class PlayerReact extends React.Component {
       duration: 0,
       playbackRate: 1.0,
       loop: false,
+
+      // List of Songs
+      songs: null,
     };
   }
 
-  load = url => { // Loads the video we wish to see
-      this.setState({
-          url,
-          played:0,
-          loaded:0,
-          pip:false
-      })
-      console.log({url});
+  componentDidMount() {
+    // Temporary hardcoded songs
+    const songs = [
+      { url: "https://www.youtube.com/watch?v=O48hUxxJxS8" },
+      { url: "https://www.youtube.com/watch?v=8B93tgRxMuE" },
+      { url: "https://www.youtube.com/watch?v=YE0WmmEn7Yk" },
+      { url: "https://www.youtube.com/watch?v=ETecZsoA0jo" },
+      { url: "https://youtu.be/8wRW57nBLMI?t=21" },
+      { url: "https://www.youtube.com/watch?v=EcCVX42H2fg" },
+      { url: "https://www.youtube.com/watch?v=bvC_0foemLY" },
+    ];
+    this.setState({ songs: songs });
+    console.log(this.state.songs);
+
+    // Load first song in list
+
+    this.setState({ url: songs[0].url });
   }
 
-  handlePlay = () => { // Play Stop Functionality
+  load = (url) => {
+    // Loads the video we wish to see
+    this.setState({
+      url,
+      played: 0,
+      loaded: 0,
+      pip: false,
+    });
+    console.log({ url });
+  };
+
+  handlePlay = () => {
+    // Play Stop Functionality
     this.setState({ playing: !this.state.playing });
     console.log(this.state.playing);
   };
 
-  handleVolumeChange = e => { // Volume
-    this.setState({ volume: parseFloat(e.target.value) })
-  }
+  handleVolumeChange = (e) => {
+    // Volume
+    this.setState({ volume: parseFloat(e.target.value) });
+  };
+
+  /*NextSong = () =>{
+      var index = this.state.songs.findIndex(urlz => urlz === this.state.url);
+      index++;
+      //var tempsong = this.state.songs[index + 1]  ;
+      //console.log("temp song is"+ tempsong.url);
+      this.load(this.state.songs[index].url);
+  }*/
 
   render() {
-      
     return (
       <>
         <div className="Player">
-          <ReactPlayer
-            ref={this.ref}
-            className="react-player"
-            width="500px"
-            height="500px"
-            url={this.state.url}
-            pip={this.state.pip}
-            playing={this.state.playing}
-            controls={this.state.controls}
-            light={this.state.light}
-            loop={this.state.loop}
-            playbackRate={this.state.playbackRate}
-            volume={this.state.volume}
-            muted={this.state.muted}
-            /*onReady={() => console.log("onReady")}
+          <div className="spinner">
+            <div className="spinerOverlay"></div>
+            <ReactPlayer
+              className={styles.reactplayer}
+              width="500px"
+              height="500px"
+              url={this.state.url}
+              pip={this.state.pip}
+              playing={this.state.playing}
+              controls={this.state.controls}
+              light={this.state.light}
+              loop={this.state.loop}
+              playbackRate={this.state.playbackRate}
+              volume={this.state.volume}
+              muted={this.state.muted}
+              /*onReady={() => console.log("onReady")}
             onStart={() => console.log("onStart")}
             onPlay={this.handlePlay}
             onEnablePIP={this.handleEnablePIP}
@@ -70,17 +107,34 @@ export default class PlayerReact extends React.Component {
             onError={(e) => console.log("onError", e)}
             onProgress={this.handleProgress}
             onDuration={this.handleDuration} */
-          />
+            />
+            
+          </div>
 
           <button onClick={this.handlePlay}>
             {this.state.playing ? "STOP" : "PLAY"}
           </button>
 
-          <button onClick={() => this.load('https://www.youtube.com/watch?v=qh3dYM6Keuw')}> Next song</button>
+          <button
+            onClick={() => {
+              var index = this.state.songs.findIndex(
+                (urlz) => urlz === this.state.url
+              );
+              index++;
+              index++;
+              this.load(this.state.songs[index].url);
+            }}
+            /*onClick={() =>
+              this.load("https://www.youtube.com/watch?v=qh3dYM6Keuw")
+            }*/
+          >
+            {" "}
+            Next song
+          </button>
 
           <div className="volume">
             <label for="customRange3" class="form-label">
-              Example range
+              Volume
             </label>
             <input
               type="range"
@@ -91,7 +145,6 @@ export default class PlayerReact extends React.Component {
               id="customRange3"
               onChange={this.handleVolumeChange}
             ></input>
-
           </div>
         </div>
       </>
